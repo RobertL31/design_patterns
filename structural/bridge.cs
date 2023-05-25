ffef154bbff77b4983a9a0a002f4e459a855cfe8
RobertL31 <loicalban@laposte.net>
2023-05-25 14:44:23 +0200

    bridge

--- structural/bridge.cs
+++ structural/bridge.cs
@@ -6,12 +6,67 @@
     while still being able to work together.
 */
 
+using System; 
 
+class Abstraction {
+
+    protected IImplemention _implementation;
+    
+    public Abstraction(IImplemention implementation){
+        this._implementation = implementation;
+    }
+    
+    public void SomeFeature() {
+        this._implementation.SomeMethod();
+    }
+
+    public void SomeOtherFeature() {
+        this._implementation.SomeOtherMethod();
+    }
+}
+
+
+class RefinedAbstraction : Abstraction {
+
+    public RefinedAbstraction(RefinedImplementation implementation)
+        : base(implementation){}
+
+    public void ExtraFeature() {
+        this._implementation.SomeMethod();
+        this._implementation.SomeOtherMethod();
+    }
+}
+
+
+interface IImplemention {
+    void SomeMethod();
+    void SomeOtherMethod();
+}
+
+
+class RefinedImplementation : IImplemention {
+    
+    public void SomeMethod() {
+        Console.WriteLine("SomeMethod");
+    }
+
+    public void SomeOtherMethod() {
+        Console.WriteLine("SomeOtherMethod");
+    }
+
+    public void ExtraMethod() {
+        Console.WriteLine("ExtraMethod");
+    }
+}
 
 
 class Client {
     public void ClientCode() {
-        
+        var implementation = new RefinedImplementation();
+        var abstraction = new RefinedAbstraction(implementation);
+        abstraction.SomeFeature();
+        abstraction.SomeOtherFeature();
+        abstraction.ExtraFeature();
     }
 }
 
